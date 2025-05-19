bbfa8cd5c54aa1deed6ace7ccba0834b217514c0
vcpersike <vcpersike@gmail.com>
2025-05-19 00:38:53 -0300

    Ajustes de estrutura

---
+++ AppGamboaSite.Shared/wwwroot/js/scrollToElement.js
@@ -0,0 +1,10 @@
+﻿window.scrollToElement = (elementId) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest'
+        });
+    }
+};
