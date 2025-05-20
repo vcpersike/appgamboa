0a3985f4b2ab98443a46bcf634ed7a29a060f773
Victor Silva <Victor.Silva.Externo@caixavidaeprevidencia.com.br>
2025-05-20 19:38:34 -0300

    Ajustes

---
+++ AppGamboaSite.Shared/Views/Pages/Landpage.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGamboaSite.Shared.Views.Pages
+{
+    public partial class Landpage
+    {
+        public async Task ScrollToElement(string elementId)
+        {
+            await JS.InvokeVoidAsync("scrollToElement", elementId);
+        }
+    }
+}
---
+++ AppGamboaSite.Shared/Views/Pages/PoliticaPrivacidade.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGamboaSite.Shared.Views.Pages
+{
+    public partial class PoliticaPrivacidade
+    {
+        public class MenuItemModel
+        {
+            public string Title { get; set; }
+            public string Icon { get; set; }
+            public string Url { get; set; }
+        }
+    }
+}
