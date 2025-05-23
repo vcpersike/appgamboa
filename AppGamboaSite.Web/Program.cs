71974876d5c129d506703a2172d785144cbd605f
Victor Silva <Victor.Silva.Externo@caixavidaeprevidencia.com.br>
2025-05-23 18:02:23 -0300

    Ajustes

--- AppGamboaSite.Web/Program.cs
+++ AppGamboaSite.Web/Program.cs
@@ -3,22 +3,16 @@ using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-// Add device-specific services used by the AppGamboaSite.Shared project
 
 builder.Services.AddMudServices();
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
